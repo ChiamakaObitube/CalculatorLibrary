7e203e405e6382c9c47c0e9ed2ebb23e15eb5105
devylawyer <obitubechiamaka@gmail.com>
2020-12-15 01:05:36 +0100

    Add function and unit test for division

--- test_calculator.py
+++ test_calculator.py
@@ -14,4 +14,7 @@ class TestCalculator:
         assert 2 == calculator.subtract(4, 2)
 
     def test_multiplication(self):
-    assert 100 == calculator.multiply(10, 10)
+        assert 100 == calculator.multiply(10, 10)
+
+    def test_division(self):
+        assert 10 == calculator.divide(100, 10)
